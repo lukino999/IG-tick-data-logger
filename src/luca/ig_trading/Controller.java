69440ef513ff2c7916890af72636c9931ca296ec
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-06-19 15:21:10 +0100

    Minor renaming

---
+++ src/luca/ig_trading/Controller.java
@@ -0,0 +1,13 @@
+package luca.ig_trading;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void connect(ActionEvent actionEvent) {
+        System.out.println("connect");
+    }
+
+    public void onMenuConnectConnect(ActionEvent actionEvent) {
+        System.out.println("onMenuConnectConnect");
+    }
+}
